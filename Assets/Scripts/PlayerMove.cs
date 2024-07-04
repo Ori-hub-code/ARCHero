1c367d60e56a73ad317f62d1a1144356da42db8e
Ori-hub-code <uic3883@gmail.com>
2024-07-04 22:25:35 +0900

    조이스틱 제작

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -13,10 +13,18 @@ public class PlayerMove : Singleton<PlayerMove>
     [SerializeField] float vAxis;
     [SerializeField] float speed;
 
+    [Header("# Scripts")]
+    JoyStick joyStick;
+
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();  
+    }
+
+    private void Start()
+    {
+        joyStick = JoyStick.Instance;
     }
 
     private void FixedUpdate()
@@ -26,20 +34,23 @@ public class PlayerMove : Singleton<PlayerMove>
 
     private void Move()
     {
-        hAxis = Input.GetAxis("Horizontal");
-        vAxis = Input.GetAxis("Vertical");
+        // hAxis = Input.GetAxis("Horizontal");
+        // vAxis = Input.GetAxis("Vertical");
 
-        rigid.velocity = new Vector3(hAxis, 0, vAxis).normalized * speed; // 좌표값
-        //rigid.rotation = Quaternion.LookRotation(new Vector3(0, ,0)); // 회전값
+        if(joyStick.joyVec.x != 0 || joyStick.joyVec.y != 0)
+        {
+            rigid.velocity = new Vector3(joyStick.joyVec.x, 0, joyStick.joyVec.y).normalized * speed; // 좌표값
+            rigid.rotation = Quaternion.Euler(0f, Mathf.Atan2(joyStick.joyVec.x, joyStick.joyVec.y) * Mathf.Rad2Deg,0f); ; // 회전값
+        }
 
 
         // 애니메이션
-        if(hAxis != 0 || vAxis != 0)
+        if(joyStick.joyVec.x != 0 || joyStick.joyVec.y != 0)
         {
             anim.SetBool("isIdle", false);
             anim.SetBool("isWalk", true);
         }
-        else if(hAxis == 0 && vAxis == 0)
+        else if(joyStick.joyVec.x == 0 && joyStick.joyVec.y == 0)
         {
             anim.SetBool("isIdle", true);
             anim.SetBool("isWalk", false);
